553993637688f8148f9c8c13c6089a137fa13e51
Vedant Chavan <75489881+vedant2412@users.noreply.github.com>
2025-11-08 23:45:32 +0000

    datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 35165
+let accountEmail = "vedant@google.com"
+var accountPassword = "Chavan"
+let accountCity
+
+accountEmail = "chavan@google.com"
+console.table([accountID, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+let name = "Vedant"
+let score = 36
+let isPresent = true
+let blank = "33r"
+let nothing = null
+// console.log(typeof null);
+console.log(typeof blank);
+// console.log(typeof isPresent);
+// console.log(typeof score);
+// console.log(typeof name);
+
+let notblank = Number(blank)
+console.log(typeof notblank);
+console.log(notblank);
